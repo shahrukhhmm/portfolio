5f825e159dd24c4e23600080cfe7204d7780ff3c
Shahrukh Mehmood <89530184+shahrukhhmm@users.noreply.github.com>
2022-08-14 11:48:41 +0500

    Add files via upload

--- js/stickynav.js
+++ js/stickynav.js
@@ -99,12 +99,9 @@ buttons.forEach(button =>{
 const body1 = document.querySelector('.body1');
 const body2 = document.querySelector('.body2');
 const body3 = document.querySelector('.body3');
-const body4 = document.querySelector('.body4');
 const inner1 = document.querySelector('.inner1');
 const inner2 = document.querySelector('.inner2');
 const inner3 = document.querySelector('.inner3');
-const inner4 = document.querySelector('.inner4');
-const inner5 = document.querySelector('.inner5');
 body1.addEventListener('click',()=>{
     inner1.classList.toggle('rotate');
 })
@@ -114,9 +111,7 @@ body2.addEventListener('click',()=>{
 body3.addEventListener('click',()=>{
     inner3.classList.toggle('rotate');
 })
-body4.addEventListener('click',()=>{
-    inner4.classList.toggle('rotate');
-})
+
 
 
 // portfolio
@@ -133,4 +128,24 @@ backbtn.addEventListener("click", () =>{
 frontbtn.addEventListener("click", () =>{
   backend.classList.add('noneornot')
   frontend.classList.remove('noneornot')
+})
+
+//aboutbtn
+const qualibtn= document.querySelector('.qualifi-btn');
+const expertbtn= document.querySelector('.experties-btn');
+const qua_box= document.querySelector('.qualify-box');
+const exp_box= document.querySelector('.expert-box');
+
+qualibtn.addEventListener('click', ()=> {
+  qua_box.style.display='flex';
+
+  exp_box.style.display='none';
+
+})
+expertbtn.addEventListener('click', ()=> {
+  
+  qua_box.style.display='none';
+
+  exp_box.style.display='flex';
+
 })
